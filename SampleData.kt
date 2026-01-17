8efdf9046272125f7ec70b98e044cb603457dc88
Atte <atte.kantola@student.oulu.fi>
2026-01-17 13:45:21 +0200

    Add files via upload

---
+++ SampleData.kt
@@ -0,0 +1,90 @@
+package com.example.navigation
+/**
+ * SampleData for Jetpack Compose Tutorial 
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Pasi",
+            "Hello There!"
+        ),
+        Message(
+            "Pasi",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Pasi",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Pasi",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Pasi",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Pasi",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Pasi",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Pasi",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Pasi",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Pasi",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Pasi",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Pasi",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Pasi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+        Message(
+            "Pasi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+        Message(
+            "Pasi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+        Message(
+            "Pasi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+        Message(
+            "Pasi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
